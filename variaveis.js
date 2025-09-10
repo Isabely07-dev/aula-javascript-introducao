6835e4ab77634575cba2110de10a0555b8554969
Isabely07-dev <isabely_reitz@estudante.sesisenai.org.br>
2025-09-10 19:31:12 -0300

    Diferenca de variáveis de portugol para JavaScript

---
+++ variaveis.js
@@ -0,0 +1,26 @@
+//Portugol para JavaScript
+//Declaração de variáveis
+
+
+/*Em portugol:
+cadeia nome = "Isa"
+cadeia sobrenome = "Soares"
+inteiro idade = 17
+real altura = 1.59
+logico ehAluno = verdadeiro 
+*/
+
+
+let nome = "Isa"
+let sobrenome = "Soares"
+let idade = 17
+let altura = 1.59
+let ehAluno = true
+
+
+
+console.log("Nome: ", nome)//escreva("Nome: ", nome)
+console.log("Sobrenome: ", sobrenome)//escreva("Sobrenome: ", sobrenome)
+console.log("Idade: ", idade)//escreva("Idade: ", idade)
+console.log("Altura: ", altura)//escreva("Altura: ", altura)
+console.log("É aluno? ", ehAluno)//escreva("É aluno? ", ehAluno)
