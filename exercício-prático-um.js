7120b4bc466cdb7b14f880f4c6bd66e097510925
Isabely07-dev <isabely_reitz@estudante.sesisenai.org.br>
2025-09-10 23:43:13 +0000

    Adiciona exercício prático com informações pessoais e logs no console

---
+++ exercício-prático-um.js
@@ -0,0 +1,8 @@
+let meuNome = "Isabely Soares Reitz";
+let minhaIdade = "17";
+let minhaCidade ="Pomerode - SC";
+let gostaDeProgramar = true;
+
+console.log(`Perfil: ${meuNome}, ${minhaIdade} anos!`);
+console.log(`Cidade: ${minhaCidade}`);
+console.log("Gosta de Programar:", gostaDeProgramar);
