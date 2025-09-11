73689b8021fbe476ae00232dffe1f225faafaffc
Isabely07-dev <isabely_reitz@estudante.sesisenai.org.br>
2025-09-11 22:02:16 +0000

    Adiciona exemplo prático de tabuada ao código de comparação

--- 7-comparação.js
+++ 7-comparação.js
@@ -11,3 +11,13 @@ while (contador <= 3) {
     console.log("contador:",contador);
     contador++;
 }
+
+//Exemplo prático - tabuada
+
+let numero = 7;
+console.log (`=== Tabuada do ${numero} ===`);
+
+for (let i = 1; i <=10; i++){
+    let resultado = numero * i;
+    console.log(`${numero} x ${i} = ${resultado}`);
+}
