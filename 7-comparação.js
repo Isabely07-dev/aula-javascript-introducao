415357d0b8eaac4bb08482d59a23289dd17584d7
Isabely07-dev <isabely_reitz@estudante.sesisenai.org.br>
2025-09-11 21:44:12 +0000

    Adiciona comentários explicativos para loops de comparação

--- 7-comparação.js
+++ 7-comparação.js
@@ -1,3 +1,13 @@
+//loop for - comparação
+
 for(let i = 1; i <= 5; i++){
  console.log("Número:", i);
+}
+
+// Loop While - comparação
+
+let contador = 1;
+while (contador <= 3) {
+    console.log("contador:",contador);
+    contador++;
 }
