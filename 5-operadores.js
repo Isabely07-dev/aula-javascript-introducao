c4903ae173108bfb70b79bd134e7c7afbd584f5b
Isabely07-dev <isabely_reitz@estudante.sesisenai.org.br>
2025-09-11 00:44:11 +0000

    Remove arquivos desnecessários: Tipos-de-dados.js, condicionais.js, exercício-prático-um.js e variaveis.js

---
+++ 5-operadores.js
@@ -0,0 +1,13 @@
+//igualdade (==, ===) e difierença (!=, !==)
+
+console.log(5 = "5") //true, apenas compara se as escritas são iguis.
+
+console.log(5 === "5") // false, compara a escrita e o tipo. Verificar o tipo de dado.
+
+console.log(5 != "5") //false, mesmo que a escrita seja (!) 
+
+console.log(5 !== "5") // true
+
+console.log(5 > 3) // true
+
+console.log(10 < 10) //false
