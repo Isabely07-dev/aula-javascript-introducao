3e61692a15d4a482baeeb4034ed738593528f28f
Isabely07-dev <isabely_reitz@estudante.sesisenai.org.br>
2025-09-17 22:42:09 +0000

    Adiciona funções de tabuada e calculadora ao código

--- 10-arrow.js
+++ 10-arrow.js
@@ -21,3 +21,30 @@ const ehParArrow = numero => numero % 2 == 0;
 console.log(ehParArrow(6));
 
 
+function tabuada(numero) {
+    for (let i = 1; i <= 5; i++) {
+        console.log(`${numero} x ${i} = ${numero * i}`);
+    }
+}
+
+console.log("---------Calculadora------------");
+
+function calculadora(n1, n2, operacao) {
+   switch (operacao){
+    case '+': return n1 + n2;
+    case '-': return n1 - n2;
+    case '*': return n1 * n2;
+    case '/': 
+     return n2 != 0 ? n1 / n2 : "Erro: Divisão por zero!";
+    default:
+         return "Operação inválida";
+   }
+}
+
+  console.log("10 + 5 =", calculadora(10, 5, '+'));
+  console.log("10 - 5 =", calculadora(10, 5, '-'));
+  console.log("10 / 0 =", calculadora(10, 5, '/'));
+  console.log("10 * 5 =", calculadora(10, 5, '*'));
+
+
+
