a8330b9d301f7888c715886416851d2dc41b06e9
Isabely07-dev <isabely_reitz@estudante.sesisenai.org.br>
2025-09-17 19:01:28 -0300

    funções de comparação

---
+++ 9-Funções-comparação.js
@@ -0,0 +1,22 @@
+function calcularMedia (n1, n2) {
+    return (n1 + n2) / 2;
+}
+
+function soma (n1, n2){
+    return n1 + n2;
+}
+
+function sub (n1, n2) { 
+return n1 - n2;
+}
+
+function mult(n1, n2){
+    return n1 * n2;
+}
+
+
+
+
+
+let media = calcularMedia(8, 7);
+console.log("Média:", media);
---
+++ arrow.js
@@ -0,0 +1,7 @@
+function somar(a, b) {
+    return a + b;
+}
+
+const somar = (a, b) => a + b;
+
+console.log("Soma:", somar(10, 5));
