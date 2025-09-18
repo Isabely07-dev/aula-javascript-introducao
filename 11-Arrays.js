3662627db6cab3afddc6a7993c90ee83d08e5dd6
Isabely07-dev <isabely_reitz@estudante.sesisenai.org.br>
2025-09-18 23:17:20 +0000

    Corrige mensagens de log para exibir "Posição" e "Fruta" corretamente no arquivo 11-Arrays.js

--- 11-Arrays.js
+++ 11-Arrays.js
@@ -1,15 +1,33 @@
 let numeros = [10, 20, 30, 40, 50];
-
 for(let i = 0; i < numeros.length; i++) {
-    console.log(`Índice ${i}:`, numeros[i]);
+    console.log(`Posição ${i}:`, numeros[i]);
 }
 
-let frutas = ["Maçã", "Banana", "Laranja"];
 
+
+let frutas = ["Maçã", "Banana", "Laranja"];
 for(let fruta of frutas) {
     console.log("Fruta:", fruta);
 }   
-
-frutas.forEach(fruta =>{
-    console.log("fruta:", fruta);
+frutas.forEach(fruta => {
+    console.log("Fruta:", fruta);
 });
+
+
+{ 
+console.log("Ana", "Bruno", "Carlos");
+
+//adiciona no inicio
+nomes.unhshift("Daniela");
+console.log(nomes);
+}
+
+{
+    let carros = ["Ford", "Chevrolet", "Toyota"];
+
+    carros.shift();
+    console.log(carros);
+
+    console.log(carros.indexOf("Toyota"));
+}
+
