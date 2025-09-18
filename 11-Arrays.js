cfcb208c464082892c23238f90d9890af5b22235
Isabely07-dev <isabely_reitz@estudante.sesisenai.org.br>
2025-09-18 22:34:44 +0000

    Adiciona arquivo index.html e vincula 11-Arrays.js ao documento

--- 11-Arrays.js
+++ 11-Arrays.js
@@ -4,3 +4,12 @@ for(let i = 0; i < numeros.length; i++) {
     console.log(`Índice ${i}:`, numeros[i]);
 }
 
+let frutas = ["Maçã", "Banana", "Laranja"];
+
+for(let fruta of frutas) {
+    console.log("Fruta:", fruta);
+}   
+
+frutas.forEach(fruta =>{
+    console.log("fruta:", fruta);
+});
