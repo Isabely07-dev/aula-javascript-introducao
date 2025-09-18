7df0c0cc00efdb8af536f442c5dacdd989a6e000
Isabely07-dev <isabely_reitz@estudante.sesisenai.org.br>
2025-09-18 23:58:06 +0000

    Remove código redundante e melhora a legibilidade no arquivo 11-Arrays.js

--- 11-Arrays.js
+++ 11-Arrays.js
@@ -1,25 +1,25 @@
 let numeros = [10, 20, 30, 40, 50];
+
 for(let i = 0; i < numeros.length; i++) {
     console.log(`Posição ${i}:`, numeros[i]);
 }
 
-
-
 let frutas = ["Maçã", "Banana", "Laranja"];
-for(let fruta of frutas) {
+
+for (let fruta of frutas) {
     console.log("Fruta:", fruta);
-}   
-frutas.forEach(fruta => {
-    console.log("Fruta:", fruta);
-});
+}
 
+{
+frutas.forEach(fruta => console.log("Fruta:", frutas));
+}
 
-{ 
-console.log("Ana", "Bruno", "Carlos");
+{
+    let nomes = ["Ana", "Bruno", "Carlos"];
 
-//adiciona no inicio
-nomes.unhshift("Daniela");
-console.log(nomes);
+    nomes.unshift("Diana");
+    console.log(nomes);
+
 }
 
 {
@@ -31,3 +31,36 @@ console.log(nomes);
     console.log(carros.indexOf("Toyota"));
 }
 
+
+
+{
+    let numeros = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    let numeros_pares = numeros.filter(n => n % 2 === 0);
+    console.log(numeros_pares);// [2, 4, 6, 8, 10]
+
+    let quadrados = numeros.map(n => n * n);
+    console.log(quadrados);// [1, 4, 9, 16, 25,..., 100]
+}
+
+{
+    let cores = ["vermelho", "verde", "azul"];
+    console.log(cores.includes("verde")); // true
+    console.log(cores.includes("amarelo")); // false
+}
+
+{
+    let frutas = ["Maçã", "Banana", "Laranja"];
+    console.log(frutas.slice("0, 2")); // ["Maçã", "Banana"]
+    
+
+    let palavras = "JavaScript";
+    console.log(palavras.slice(0, 4)); // "Java"
+
+}
+
+{
+    let none = "Isa";
+    console.log(none.toLocaleLowerCase()); // "isa"
+    console.log(none.toLocaleUpperCase()); // "ISA"
+}
